3269dd798605de9dfacdd1dcc983cd6d749ab49e
luthfi ferdian <luthfi14ferdian@gmail.com>
2024-01-26 17:04:30 +0700

    first commit

---
+++ src/main/java/org/digitalsekola/App.java
@@ -0,0 +1,20 @@
+package org.digitalsekola;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
