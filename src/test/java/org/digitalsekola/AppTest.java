5ffb651bbb4f0b15c33d2825d1d6db124be1f009
luthfi ferdian <luthfi14ferdian@gmail.com>
2024-01-28 07:11:28 +0700

    add materi 5

--- src/test/java/org/digitalsekola/AppTest.java
+++ src/test/java/org/digitalsekola/AppTest.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
@@ -14,21 +18,24 @@ class AppTest{
     @Test
     public void TestCase1(){
         WebDriver driver = new FirefoxDriver();
-
         driver.get("https://yopmail.com/en/");
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement inputBar = driver.findElement(By.className("ycptinput"));
+        inputBar.sendKeys("testing");
+        WebElement enterKey = driver.findElement(By.xpath("//button[@class=\"md\"]"));
+        enterKey.click();
+        WebElement titleEmail = driver.findElement(By.className("bname"));
 
-        driver.getTitle();
+//        Assert.assertEquals(titleEmail.getText(),"testing@yopmail.com!");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(titleEmail.getText(),"testing@yopmail.com!");
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
-//        WebElement textBox = driver.findElement(By.name("my-text"));
-//        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-//
-//        textBox.sendKeys("Selenium");
-//        submitButton.click();
-//
-//        WebElement message = driver.findElement(By.id("message"));
-//        message.getText();
+//        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+//        wait.until(d -> inputBar.isDisplayed());
+
+
         driver.quit();
+        softAssert.assertAll();
     }
 }
